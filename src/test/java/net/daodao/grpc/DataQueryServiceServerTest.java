5c55e94ebf42450b211a86291ce6bfda22d596c2
William-hu0901 <delong201314@gmail.com>
2025-10-10 16:55:01 +0800

    add test case

---
+++ src/test/java/net/daodao/grpc/DataQueryServiceServerTest.java
@@ -0,0 +1,47 @@
+package net.daodao.grpc;
+
+import io.grpc.Server;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataQueryServiceServerTest {
+    private Server server;
+    private DataQueryServiceGrpc.DataQueryServiceBlockingStub blockingStub;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        String serverName = InProcessServerBuilder.generateName();
+        server = InProcessServerBuilder.forName(serverName)
+                .directExecutor()
+                .addService(new DataQueryServiceServer.DataQueryServiceImpl())
+                .build()
+                .start();
+
+        blockingStub = DataQueryServiceGrpc.newBlockingStub(
+                InProcessChannelBuilder.forName(serverName)
+                        .directExecutor()
+                        .build());
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        if (server != null) {
+            server.shutdownNow().awaitTermination();
+        }
+    }
+
+    @Test
+    public void testQueryData() {
+        DataQueryRequest request = DataQueryRequest.newBuilder()
+                .setQuery("Test query")
+                .build();
+
+        DataQueryResponse response = blockingStub.queryData(request);
+        assertEquals("Processed query: Test query", response.getResult());
+    }
+}
